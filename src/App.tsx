074c7eabb82e4dcfd4f6d6c57dcafa48b99b4588
giga64 <marcusvmdr64@gmail.com>
2025-08-20 23:17:16 -0300

    xxxxxxxxxxxxxxx

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import AssistantChat from "./pages/AssistantChat";
+import NotFound from "./pages/NotFound";
+import AuthWrapper from "./components/AuthWrapper";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<AuthWrapper><Dashboard /></AuthWrapper>} />
+          <Route path="/assistant/:sector/:assistant" element={<AuthWrapper><AssistantChat /></AuthWrapper>} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
